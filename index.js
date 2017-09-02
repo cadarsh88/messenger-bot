fa694c208615fb612704efb2d19040c30b7b136b
Adarsh <cadarsh88@gmail.com>
2017-09-02 19:40:11 +0530

    Added some Functions
    Added some Functions

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/', function (req, res) {
 
 // for facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+	if (req.query['hub.verify_token'] === 'world_full_of_hopelessness_verify_me') {
 		res.send(req.query['hub.challenge'])
 	} else {
 		res.send('Error, wrong token')
@@ -139,3 +139,36 @@ function sendGenericMessage(sender) {
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+	    let event = req.body.entry[0].messaging[i]
+	    let sender = event.sender.id
+	    if (event.message && event.message.text) {
+		    let text = event.message.text
+		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+	    }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAAbShItNAm4BADRvJ0y3pignALZCPH3Bq1bQob9SarmEUNX6lZCFNCW5q5bQC1120FZBh5gLMdt99b5aekX2XjlM7v9L9fsOghu5OEREu2DDYTdjQrYqn85ClIC9CQM0giSjv2nkTGmM9eZAVqBa0XmYeXCL4obsBLm84Ort3AZDZD"
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+		json: {
+		    recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+		    console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    })
+}
